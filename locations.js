5a8f2ec772ddc41466bae18ded1f492f65c971ca
ShuzoN <d.namu.r21@gmail.com>
2020-10-28 19:19:35 +0900

    add city

--- locations.js
+++ locations.js
@@ -1,6 +1,6 @@
 exports.getIdByName = function (locationName) {
   var map = new Map();
-  
+
   map.set("千代田", 13101);
   map.set("中央", 13102);
   map.set("港", 13103);
@@ -24,6 +24,32 @@ exports.getIdByName = function (locationName) {
   map.set("足立", 13121);
   map.set("葛飾", 13122);
   map.set("江戸川", 13123);
+  map.set("八王子",13201);
+  map.set("立川",13202);
+  map.set("武蔵野",13203);
+  map.set("三鷹",13204);
+  map.set("青梅",13205);
+  map.set("府中",13206);
+  map.set("昭島",13207);
+  map.set("調布",13208);
+  map.set("町田",13209);
+  map.set("小金井",13210);
+  map.set("小平",13211);
+  map.set("日野",13212);
+  map.set("東村山",13213);
+  map.set("国分寺",13214);
+  map.set("国立",13215);
+  map.set("福生",13218);
+  map.set("狛江",13219);
+  map.set("東大和",13220);
+  map.set("清瀬",13221);
+  map.set("東久留米",13222);
+  map.set("武蔵村山",13223);
+  map.set("多摩",13224);
+  map.set("稲城",13225);
+  map.set("羽村",13227);
+  map.set("あきる野",13228);
+  map.set("西東京",13229);
   map.set("沖縄", 47211);
 
   return map.get(locationName);
