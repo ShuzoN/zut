38926b6f7037876ba4c61da746d860febffc290d
ShuzoN <d.namu.r21@gmail.com>
2020-11-01 10:31:19 +0900

    lambda getbodyのテスト追加

---
+++ test/lambda.test.js
@@ -0,0 +1,15 @@
+const lambda = require('../lambda');
+
+test("lambdaに送信されるeventからbodyをobjectとして取り出せること", () => {
+    const event = {
+        'body-json': {
+        body: 'token=token&command=/zut&text=杉並'
+      }
+    };
+
+    expect({
+        token: "token",
+        command: "/zut",
+        text: "杉並"
+    }).toEqual(expect.objectContaining(lambda.getBody(event)));
+});
