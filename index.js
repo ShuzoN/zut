e7ac61bd398f6e3a18107c34b10e5cbd21efd9bb
ShuzoN <d.namu.r21@gmail.com>
2021-03-14 10:45:41 +0900

    tommorowの綴りを間違えていた

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ exports.handler = async (event, context, callback) => {
   const body = lambda.getBody(event);
   const args = body.text.split(" ");
   const locationName = args[0];
-  const isTomorrow = args[1].includes("--tomorrow");
+  const isTommorow = args[1].includes("--tommorow");
   if (
     locationName.includes("--") ||
     locations.getIdByName(locationName) === undefined
   ) {
-    const responseBody = `/zut 場所　(--tomorrow)
+    const responseBody = `/zut 場所　(--tommorow)
 地域を指定してください: ${locations.getArrayList().join(",")}`;
     return slack.buildResponse(responseBody);
   }
@@ -20,7 +20,7 @@ exports.handler = async (event, context, callback) => {
   const locationId = locations.getIdByName(locationName);
 
   return await zutool.fetch(locationId).then((response) => {
-    const day = isTomorrow ? response.tomorrow : response.today;
+    const day = isTommorow ? response.tommorow : response.today;
     const responseBody = zutool.formatter(day).join("\n");
     return slack.buildResponse(responseBody);
   });
