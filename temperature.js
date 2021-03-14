0e9cad9e127cfea050279a02e9142fe35421c8bf
ShuzoN <d.namu.r21@gmail.com>
2021-03-14 12:37:11 +0900

    名前空間に違和感があったので移動

---
+++ temperature.js
@@ -0,0 +1,56 @@
+exports.inspectDifference = function (before, after) {
+  const difference = (a, b) => {
+    return Math.abs(a - b);
+  };
+
+  const diff = difference(before, after);
+
+  if (diff >= 5 && diff < 8) {
+    return 1;
+  }
+
+  if (diff >= 8 && diff < 10) {
+    return 2;
+  }
+
+  if (diff >= 10) {
+    return 3;
+  }
+
+  return 0;
+};
+
+exports.format = function (diff) {
+  switch (diff) {
+    case 0:
+      return "";
+    case 1:
+      return "前日比の気温差が5度以上。体調を崩しやすい日 :waning:";
+    case 2:
+      return "前日比の気温差が8度以上。体調を崩していてもおかしくありません :bomb:";
+    case 3:
+      return "前日比の気温差が10度以上。体調を崩しているなら休みましょう :boom:";
+    default:
+      return "";
+  }
+};
+
+exports.daysMax = function (json) {
+  return {
+    yesterday: max(listTemperatureInADay(json.yesterday)),
+    today: max(listTemperatureInADay(json.today)),
+    tomorrow: max(listTemperatureInADay(json.tommorow)),
+  };
+};
+
+function listTemperatureInADay(day) {
+  return day.map((d) => {
+    return d.temp;
+  });
+}
+
+function max(dayTemp) {
+  return dayTemp.reduce(function (a, b) {
+    return Math.max(a, b);
+  });
+}
---
+++ test/temperature.test.js
@@ -0,0 +1,163 @@
+const temperature = require("../temperature");
+
+test("昨日、今日、明日の最高気温を取得する", () => {
+  const expected = {
+    yesterday: 13.8,
+    today: 20.9,
+    tomorrow: 19.5,
+  };
+
+  expect(temperature.daysMax(zutoolJson)).toEqual(expected);
+});
+
+test("昨日、今日の気温差が5度以下の場合は0が返ること", () => {
+  const yesterday = 15.1;
+  const today = 20.0;
+
+  expect(temperature.inspectDifference(yesterday, today)).toEqual(0);
+});
+
+test("マイナスの場合でも昨日、今日の気温差が5度以下の場合は0が返ること", () => {
+  const yesterday = 0;
+  const today = -4.9;
+
+  expect(temperature.inspectDifference(yesterday, today)).toEqual(0);
+});
+
+test("昨日、今日の気温差が5度以上8度未満の場合は1が返ること", () => {
+  const yesterday = 15.0;
+  const today = 20.0;
+
+  expect(temperature.inspectDifference(yesterday, today)).toEqual(1);
+
+  const yesterday2 = 12.1;
+  const today2 = 20.0;
+
+  expect(temperature.inspectDifference(yesterday2, today2)).toEqual(1);
+});
+
+test("マイナスの場合でも昨日、今日の気温差が5度以上8度未満の場合は1が返ること", () => {
+  const yesterday = 0;
+  const today = -5;
+
+  expect(temperature.inspectDifference(yesterday, today)).toEqual(1);
+
+  const yesterday2 = 5.0;
+  const today2 = -2.9;
+
+  expect(temperature.inspectDifference(yesterday2, today2)).toEqual(1);
+});
+
+test("昨日、今日の気温差が8度以上10度未満の場合は2が返ること", () => {
+  const yesterday = 12.0;
+  const today = 20.0;
+
+  expect(temperature.inspectDifference(yesterday, today)).toEqual(2);
+
+  const yesterday2 = 10.1;
+  const today2 = 20.0;
+
+  expect(temperature.inspectDifference(yesterday2, today2)).toEqual(2);
+});
+
+test("マイナスの場合でも昨日、今日の気温差が8度以上10度未満の場合は2が返ること", () => {
+  const yesterday = 3;
+  const today = -5;
+
+  expect(temperature.inspectDifference(yesterday, today)).toEqual(2);
+
+  const yesterday2 = 5.0;
+  const today2 = -4.9;
+
+  expect(temperature.inspectDifference(yesterday2, today2)).toEqual(2);
+});
+
+test("昨日、今日の気温差が10度以上の場合は3が返ること", () => {
+  const yesterday = 9.9;
+  const today = 20.0;
+
+  expect(temperature.inspectDifference(yesterday, today)).toEqual(3);
+});
+
+test("マイナスの場合でも昨日、今日の気温差が10度以上の場合は3が返ること", () => {
+  const yesterday = 5.0;
+  const today = -6.0;
+
+  expect(temperature.inspectDifference(yesterday, today)).toEqual(3);
+});
+
+const zutoolJson = {
+  place_name: "東京都杉並区",
+  place_id: "115",
+  prefectures_id: "13",
+  dateTime: "2020-11-01 10",
+  yesterday: [
+    {
+      time: "0",
+      weather: "300",
+      temp: "12.0",
+      pressure: "1018",
+      pressure_level: "4",
+    },
+    {
+      time: "1",
+      weather: "300",
+      temp: "13.8",
+      pressure: "1004.6",
+      pressure_level: "4",
+    },
+    {
+      time: "2",
+      weather: "100",
+      temp: "9.6",
+      pressure: "1002.5",
+      pressure_level: "1",
+    },
+  ],
+  today: [
+    {
+      time: "0",
+      weather: "100",
+      temp: "10.6",
+      pressure: "1027.7",
+      pressure_level: "1",
+    },
+    {
+      time: "1",
+      weather: "100",
+      temp: "20.9",
+      pressure: "1024.1",
+      pressure_level: "3",
+    },
+    {
+      time: "2",
+      weather: "100",
+      temp: "12.8",
+      pressure: "1023.6",
+      pressure_level: "2",
+    },
+  ],
+  tommorow: [
+    {
+      time: "0",
+      weather: "100",
+      temp: "8",
+      pressure: "1011.7",
+      pressure_level: "1",
+    },
+    {
+      time: "1",
+      weather: "100",
+      temp: "19.5",
+      pressure: "1013.5",
+      pressure_level: "0",
+    },
+    {
+      time: "2",
+      weather: "100",
+      temp: "9.7",
+      pressure: "1013.9",
+      pressure_level: "4",
+    },
+  ],
+};
