48674ccb1e878fc212fe1004d4ea203c339ac482
ShuzoN <d.namu.r21@gmail.com>
2021-03-14 22:56:06 +0900

    検索結果に応じて出し分けした

--- index.js
+++ index.js
@@ -9,26 +9,40 @@ exports.handler = async (event, context, callback) => {
   const args = body.text.split(" ");
   const locationName = args[0];
   const isTomorrow = args[1] ? args[1].includes("--tomorrow") : false;
-  if (
-    locationName.includes("--") ||
-    locations.getIdByName(locationName) === undefined
-  ) {
+  if (locationName === undefined || locationName.includes("--")) {
     const responseBody = `/zut 場所　(--tomorrow)
 地域を指定してください: ${locations.getArrayList().join(",")}`;
     return slack.buildResponse(responseBody);
   }
 
-  const locationId = locations.getIdByName(locationName);
+  try {
+    const result = search.byLocationName(locationName);
 
-  return await zutool.fetch(locationId).then((response) => {
-    //notice: zutoolのtomorrowの綴りが間違っているのでそちらに合わせています
-    const day = isTomorrow ? response.tommorow : response.today;
-    const dayStr = isTomorrow ? "明日" : "今日";
-    const responseBody = `${dayStr} の天気
+    if (result.length > 1) {
+      const names = result.map((r) => r.name).join("\n");
+      return slack.buildResponse(`対象住所は複数該当します。
+    ${names}`);
+    }
+
+    if (result.length < 1) {
+      const responseBody = `検索に失敗しているのでやり直してください.`;
+      return slack.buildResponse(responseBody);
+    }
+
+    const locationId = result[0].city_code;
+
+    return await zutool.fetch(locationId).then((response) => {
+      //notice: zutoolのtomorrowの綴りが間違っているのでそちらに合わせています
+      const day = isTomorrow ? response.tommorow : response.today;
+      const dayStr = isTomorrow ? "明日" : "今日";
+      const responseBody = `${dayStr} の天気
 ${zutool.formatter(day).join("\n")}
 ${temperatureDiffMessage(response, isTomorrow)}`;
-    return slack.buildResponse(responseBody);
-  });
+      return slack.buildResponse(responseBody);
+    });
+  } catch (e) {
+    return slack.buildResponse(e);
+  }
 };
 
 function temperatureDiffMessage(json, isTomorrow) {
