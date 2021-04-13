525dd6f11608ea9e49425d4ff67a6d05e197ea00
IkezoeMakoto <150283t@gmail.com>
2021-04-13 13:40:55 +0900

    city_codeでも検索したい

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const temperature = require("temperature");
 exports.handler = async (event, context, callback) => {
   const body = lambda.getBody(event);
   const args = body.text.split(" ");
-  const locationName = args[0];
-  const isTomorrow = args[1] ? args[1].includes("--tomorrow") : false;
   if (args[0] === "" || locationName.includes("--")) {
     const responseBody = `/zut 場所　(--tomorrow)
 対応する地名が複数ある場合は地名とcity_codeを表示します。
@@ -44,21 +42,31 @@ e.g. /zut 13117
     return slack.buildResponse(responseBody);
   }
 
-  const result = await search.byLocationName(locationName);
+  let locationId;
+  // locationIdは数値5桁(prefecturesId2桁 + placeId3桁)のみ指定されている場合
+  const gotLocationId = /^\d{5}$/.test(args[0]);
+  if (gotLocationId) {
+    locationId = args[0];
+  } else {
+    // locationId指定じゃない＝地域名の指定の場合は、一度検索してlocationIdを取得する
+    const locationName = args[0];
+    const result = await search.byLocationName(locationName);
 
-  if (result.length > 1) {
-    const [names, city_code] = result.map((r) => [r.name, r.city_code]).join("\n");
-    return slack.buildResponse(`対象住所は複数該当します。天気表示は「市町村区名」のみ入力してください。東京都の場合はcodeで検索してください。
-${names}: ${city_code}`);
+    if (result.length > 1) {
+      const [names, city_code] = result.map((r) => [r.name, r.city_code]).join("\n");
+      return slack.buildResponse(`対象住所は複数該当します。天気表示は「市町村区名」のみ入力してください。東京都の場合はcodeで検索してください。
+  ${names}: ${city_code}`);
+    }
+
+    if (result.length < 1) {
+      const responseBody = `検索に失敗しているのでやり直してください.`;
+      return slack.buildResponse(responseBody);
+    }
+
+    locationId = result[0].city_code;
   }
 
-  if (result.length < 1) {
-    const responseBody = `検索に失敗しているのでやり直してください.`;
-    return slack.buildResponse(responseBody);
-  }
-
-  const locationId = result[0].city_code;
-
+  const isTomorrow = args[1] ? args[1].includes("--tomorrow") : false;
   return await zutool.fetch(locationId).then((response) => {
     //notice: zutoolのtomorrowの綴りが間違っているのでそちらに合わせています
     const day = isTomorrow ? response.tommorow : response.today;
