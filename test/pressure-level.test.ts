0ca4fa86a7b265494ca067db38187159ef8522cd
ShuzoN <d.namu.r21@gmail.com>
2021-04-29 12:22:50 +0900

    testの実行

---
+++ test/pressure-level.test.ts
@@ -0,0 +1,10 @@
+import * as pressureLevel from "../pressure-level";
+
+test("警戒度に対応した数値を入力したとき、警戒度アイコンを返すこと", () => {
+  expect(pressureLevel.get("0")).toBe(":ok:");
+  expect(pressureLevel.get("1")).toBe(":ok:");
+  expect(pressureLevel.get("2")).toBe(":arrow_heading_down:");
+  expect(pressureLevel.get("3")).toBe(":warning:");
+  expect(pressureLevel.get("4")).toBe(":bomb:");
+  expect(pressureLevel.get("10")).toBe(":innocent:");
+});
