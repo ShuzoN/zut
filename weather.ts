28fd2525f6cf90c8f94a598e0593db3c3f728063
ShuzoN <d.namu.r21@gmail.com>
2021-04-29 10:04:54 +0900

    weather

---
+++ weather.ts
@@ -0,0 +1,13 @@
+export const get = (weatherType) => {
+  if (weatherType === "100") {
+    return ":sunny:";
+  }
+  if (weatherType === "200") {
+    return ":cloud:";
+  }
+  if (weatherType === "300") {
+    return ":umbrella:";
+  }
+
+  return ":innocent:";
+};
