ab69fbe17e9635049f9a3c7dc390b6a88547b64f
mii288 <atteeen@gmail.com>
2021-04-29 10:32:12 +0900

    refactor: rename `gotLocationId` -> `isLocationId`

--- index.js
+++ index.js
@@ -43,10 +43,10 @@ function parseBody(body) {
   // 引数がないもしくは'--'が入ってるときはhelp
   const isHelp = args[0] === "" || args[0].includes("--");
   // locationIdは数値5桁(prefecturesId2桁 + placeId3桁)のみ指定されている場合
-  const gotLocationId = /^\d{5}$/.test(args[0]);
-  const locationId = gotLocationId ? args[0] : null;
+  const isLocationId = /^\d{5}$/.test(args[0]);
+  const locationId = isLocationId ? args[0] : null;
   // locationIdがない場合はlocationNameとして扱う
-  const locationName = !gotLocationId ? args[0] : "";
+  const locationName = !isLocationId ? args[0] : "";
   const isTomorrow = args[1] ? args[1].includes("--tomorrow") : false;
 
   return { isHelp, locationId, locationName, isTomorrow };
