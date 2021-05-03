c927f10ccc3ef0ebc81832400dc2edbb0f9b54cb
ShuzoN <d.namu.r21@gmail.com>
2021-05-03 09:05:55 +0900

    bodyが渡ってこない場合はエラーを返す

--- index.ts
+++ index.ts
@@ -10,46 +10,56 @@ import { LocationIdResult } from "./Types/locations";
 import { DaysWeather, LocationWeatherResponse } from "./Types/zutool";
 
 exports.handler = async (event: TODO, context: TODO, callback: TODO) => {
-  const parsedBody: ParseBody = parseBody(lambda.getBody(event));
+  try {
+    const parsedBody: ParseBody = parseBody(lambda.getBody(event));
 
-  if (parsedBody.isHelp) {
-    return slack.buildResponse(help.message);
-  }
+    if (parsedBody.isHelp) {
+      return slack.buildResponse(help.message);
+    }
 
-  const fetchLocation: LocationIdResult = parsedBody.locationId
-    ? { locationId: parsedBody.locationId, errorMessage: null }
-    : await location.fetchLocationId(parsedBody.locationName);
+    const fetchLocation: LocationIdResult = parsedBody.locationId
+      ? { locationId: parsedBody.locationId, errorMessage: null }
+      : await location.fetchLocationId(parsedBody.locationName);
 
-  if (fetchLocation.errorMessage) {
-    return slack.buildResponse(fetchLocation.errorMessage);
-  }
+    if (fetchLocation.errorMessage) {
+      return slack.buildResponse(fetchLocation.errorMessage);
+    }
 
-  return await zutool
-    .fetch(fetchLocation.locationId)
-    .then((response: LocationWeatherResponse) => {
-      const daysWeather: DaysWeather = {
-        yesterday: response.yesterday,
-        today: response.today,
-        tomorrow: response.tommorow,
-        dayAfterTomorrow: response.dayaftertomorrow,
-      };
+    return await zutool
+      .fetch(fetchLocation.locationId)
+      .then((response: LocationWeatherResponse) => {
+        const daysWeather: DaysWeather = {
+          yesterday: response.yesterday,
+          today: response.today,
+          tomorrow: response.tommorow,
+          dayAfterTomorrow: response.dayaftertomorrow,
+        };
 
-      // notice: zutoolのtomorrowの綴りが間違っているのでそちらに合わせています
-      const day = parsedBody.isTomorrow
-        ? daysWeather.tomorrow
-        : daysWeather.today;
-      const dayStr = parsedBody.isTomorrow ? "明日" : "今日";
+        // notice: zutoolのtomorrowの綴りが間違っているのでそちらに合わせています
+        const day = parsedBody.isTomorrow
+          ? daysWeather.tomorrow
+          : daysWeather.today;
+        const dayStr = parsedBody.isTomorrow ? "明日" : "今日";
 
-      const responseBody: string = `${dayStr} の天気
+        const responseBody: string = `${dayStr} の天気
 ${zutool.formatter(day).join("\n")}
 ${temperature.diffMessage(daysWeather, parsedBody.isTomorrow)}`;
 
-      return slack.buildResponse(responseBody);
-    });
+        return slack.buildResponse(responseBody);
+      });
+  } catch (e) {
+    return slack.buildResponse(e);
+  }
 };
 
 function parseBody(body: LambdaBody): ParseBody {
-  const args = body.text.split(" ");
+  const args = body.text?.split(" ");
+  if (args === undefined) {
+    throw new Error(
+      "引数が正しく渡されていません。もう一度helpを見てください。"
+    );
+  }
+
   // 引数がないもしくは'--'が入ってるときはhelp
   const isHelp = args[0] === "" || args[0].includes("--");
   // locationIdは数値5桁(prefecturesId2桁 + placeId3桁)のみ指定されている場合
