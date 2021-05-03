3881469c9f17721421c8b6c94a7670f2b7b9ea40
ShuzoN <d.namu.r21@gmail.com>
2021-05-03 10:59:32 +0900

    test run

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  roots: ["./"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  clearMocks: true,
   testPathIgnorePatterns: ["dist"],
 };
