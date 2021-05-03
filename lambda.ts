65dc79bb4385ebe50a6505816585279cf2ad030d
ShuzoN <d.namu.r21@gmail.com>
2021-05-03 09:31:51 +0900

    URLSearchParamsのコンパイルが通るように

--- lambda.ts
+++ lambda.ts
@@ -1,6 +1,8 @@
 import { LambdaBody } from "./Types/lambda";
+import { TODO } from "./Types/utils";
+import { URLSearchParams } from "url";
 
-export const getBody = (event): LambdaBody => {
+export const getBody = (event: TODO): LambdaBody => {
   const paramString = event["body-json"]["body"];
   const body = [...new URLSearchParams(paramString).entries()].reduce(
     (obj, e) => ({ ...obj, [e[0]]: e[1] }),
