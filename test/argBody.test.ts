ee1bed6bff08c0068d1d99ae969d3a363764f82b
ca00740 <ca00740@M-Q2JX3KQ5CX.voyagegroup.local>
2022-05-10 15:25:31 +0900

    全角スペースのテストを追加

--- test/argBody.test.ts
+++ test/argBody.test.ts
@@ -12,3 +12,15 @@ test("bodyがundefinedの場合は例外を吐く", async () => {
 
   expect(() => argBody.parse(body)).toThrowError(expectError);
 });
+
+test("bodyに全角スペースが含まれる場合は例外を吐く", async () => {
+  const body: LambdaBody = {
+    text: "　01101",
+  };
+
+  const expectError = new Error(
+    "全角スペースが含まれています。引数から削除してください。"
+  );
+
+  expect(() => argBody.parse(body)).toThrowError(expectError);
+});
