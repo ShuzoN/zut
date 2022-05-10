8a1ee3881eb2506a134d0be3dcb6a8c09df4b48d
ca00740 <ca00740@M-Q2JX3KQ5CX.voyagegroup.local>
2022-05-10 11:07:31 +0900

    argsのパース時にtrimする

--- src/argBody.ts
+++ src/argBody.ts
@@ -11,11 +11,13 @@ export const parse = (body: LambdaBody): ParseBody => {
 
   // 引数がないもしくは'--'が入ってるときはhelp
   const isHelp = args[0] === "" || args[0].includes("--");
+  // 空白の除去
+  const trimed = args[0].trim();
   // locationIdは数値5桁(prefecturesId2桁 + placeId3桁)のみ指定されている場合
-  const gotLocationId = /^\d{5}$/.test(args[0]);
-  const locationId = gotLocationId ? args[0] : null;
+  const gotLocationId = /^\d{5}$/.test(trimed);
+  const locationId = gotLocationId ? trimed : null;
   // locationIdがない場合はlocationNameとして扱う
-  const locationName = !gotLocationId ? args[0] : "";
+  const locationName = !gotLocationId ? trimed : "";
   const isTomorrow = args[1] ? args[1].includes("--tomorrow") : false;
 
   return { isHelp, locationId, locationName, isTomorrow };
