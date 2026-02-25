777e2f290f83996af2a9be7925a26c5436f73101
ShuzoN <d.namu.r21@gmail.com>
2026-02-25 23:26:39 +0900

    use_lambda_direct_without_api_gateway

--- src/lambda.ts
+++ src/lambda.ts
@@ -3,11 +3,13 @@ import { ParseBody, TODO } from "./Types/utils";
 import { URLSearchParams } from "url";
 
 export const getBody = (event: TODO): LambdaBody => {
-  const paramString = event["body-json"]["body"];
+  const rawBody: string = event.body;
+  const paramString = event.isBase64Encoded
+    ? Buffer.from(rawBody, "base64").toString("utf-8")
+    : rawBody;
   const body = [...new URLSearchParams(paramString).entries()].reduce(
     (obj, e) => ({ ...obj, [e[0]]: e[1] }),
     {}
   );
-
   return body;
 };
